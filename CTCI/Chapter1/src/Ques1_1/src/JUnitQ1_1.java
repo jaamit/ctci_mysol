a6f72a3f4f5d16db9a121d490f8afdc2e625fc5e
Amit Jain <amit.hpepc@gmail.com>
2016-02-25 23:37:21 +0530

    Parameterized tests added
    Added following Parameterized tests 
    			{"abcde", true},
    			{"kite", true},
    			{"padle", true},
    			{"hello", false},
    			{"apple", false}

--- CTCI/Chapter1/src/Ques1_1/src/JUnitQ1_1.java
+++ CTCI/Chapter1/src/Ques1_1/src/JUnitQ1_1.java
@@ -1,9 +1,41 @@
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class JUnitQ1_1 {		
-	   @Test
-	   public void testisUniqueChars() {	  
-	      assertEquals(true,Question1_1.isUniqueChars("abcde"));
-	   }
-}
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class JUnitQ1_1 {	
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+			{"abcde", true},
+			{"kite", true},
+			{"padle", true},
+			{"hello", false},
+			{"apple", false}
+		});
+		
+	}
+	
+	@Parameter
+	public String fInput;
+	
+	@Parameter(value = 1)
+	public boolean fExpected;
+	
+	@Test
+	public void testisUniqueChars() {
+		assertEquals(fExpected,Question1_1.isUniqueChars(fInput));
+		}
+	
+	@Test
+	public void testisUniqueChars2() {
+		assertEquals(fExpected,Question1_1.isUniqueChars2(fInput));
+		}
+	}
